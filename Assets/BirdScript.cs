9af0fd2156d16ba66d6377873ea36bb797c6fcb0
AmandaEng-dev <112723018+AmandaEng-dev@users.noreply.github.com>
2024-03-16 18:57:07 -0500

    FlapStrength Code

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    public float flapStrength;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,10 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //every frame the game is checking hey has the  space bar has just been pressed, then make the bird jumo if so
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
     }
 }
